c253893971e0a1c6285f4d31c42cfe346c4eb7d2
Will Willems <rut.willems@gmail.com>
2026-01-20 18:00:52 +0100

    add base url to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routes as autoRoutes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: autoRoutes,
 })
 
